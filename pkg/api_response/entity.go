3f5f50044073c49b7cd5692fae1841d6171cf40f
Rizqi Wijaya <rizwijaya@github.com>
2023-03-23 10:24:37 +0700

    Create custom package response API.

---
+++ pkg/api_response/entity.go
@@ -0,0 +1,12 @@
+package api
+
+type Response struct {
+	Meta Meta        `json:"meta"`
+	Data interface{} `json:"data"`
+}
+
+type Meta struct {
+	Message string `json:"message"`
+	Code    int    `json:"code"`
+	Status  string `json:"status"`
+}
---
+++ pkg/api_response/response.go
@@ -0,0 +1,16 @@
+package api
+
+func APIResponse(message string, code int, status string, data interface{}) Response {
+	meta := Meta{
+		Message: message,
+		Code:    code,
+		Status:  status,
+	}
+
+	jsonResponse := Response{
+		Meta: meta,
+		Data: data,
+	}
+
+	return jsonResponse
+}
