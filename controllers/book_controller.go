541506ffd60f25afae0718848093a5c8f8d6c590
Rizqi Wijaya <rizwijaya@github.com>
2023-03-23 10:31:01 +0700

    Create custom response for get all books.

--- controllers/book_controller.go
+++ controllers/book_controller.go
@@ -1,10 +1,14 @@
 package controllers
 
 import (
+	api "bookLib/pkg/api_response"
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
 func (bc *BookController) GetBooks(c *gin.Context) {
 	book := bc.BookUseCase.AllBooks()
-	c.JSON(200, book)
+	response := api.APIResponse("Success to get all books!", http.StatusOK, "success", book)
+	c.JSON(http.StatusOK, response)
 }
