6cfab952331e4157a4d4387f56a0dbfba935ad36
Rizqi Wijaya <rizwijaya@github.com>
2023-04-20 12:06:11 +0700

    Create Unit Testing Update Books

--- modules/v1/book/usecases/book_test.go
+++ modules/v1/book/usecases/book_test.go
@@ -234,3 +234,109 @@ func TestBookUseCase_CreateBook(t *testing.T) {
 		})
 	}
 }
+
+func TestBookUseCase_UpdateBook(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	f := faketime.NewFaketime(2022, time.November, 27, 11, 30, 01, 0, time.UTC)
+	defer f.Undo()
+	f.Do()
+	location, err := time.LoadLocation("Asia/Jakarta")
+	assert.NoError(t, err)
+	now := time.Now().In(location)
+	tests := []struct {
+		nameTest   string
+		id         string
+		input      domain.UpdateBook
+		result     domain.Book
+		wantErr    bool
+		repository func(repo *m_repo.MockRepositoryPresenter)
+	}{
+		{
+			nameTest: "Test Case 1 Update Book: Success",
+			id:       "1",
+			input: domain.UpdateBook{
+				Name_book: "The Lord of the Rings",
+				Author:    "J. R. R. Tolkien",
+			},
+			result: domain.Book{
+				ID:         1,
+				Name_book:  "The Lord of the Rings",
+				Author:     "J. R. R. Tolkien",
+				Created_at: now,
+				Updated_at: now,
+			},
+			repository: func(repo *m_repo.MockRepositoryPresenter) {
+				repo.EXPECT().GetBookByID("1").Return(domain.Book{
+					ID:        1,
+					Name_book: "The Lord of the Rings",
+				}, nil)
+				repo.EXPECT().UpdateBook("1", domain.Book{
+					Name_book:  "The Lord of the Rings",
+					Author:     "J. R. R. Tolkien",
+					Updated_at: now,
+				},
+				).Return(domain.Book{
+					ID:         1,
+					Name_book:  "The Lord of the Rings",
+					Author:     "J. R. R. Tolkien",
+					Created_at: now,
+					Updated_at: now,
+				}, nil)
+			},
+		},
+		{
+			nameTest: "Test Case 2 Update Book: Failed, Book Not Found",
+			id:       "2",
+			input: domain.UpdateBook{
+				Name_book: "Harry Potter",
+				Author:    "J. K. Rowling",
+			},
+			result:  domain.Book{},
+			wantErr: true,
+			repository: func(repo *m_repo.MockRepositoryPresenter) {
+				repo.EXPECT().GetBookByID("2").Return(domain.Book{}, errors.New("record not found"))
+			},
+		},
+		{
+			nameTest: "Test Case 2 Update Book: Failed Update Book",
+			id:       "3",
+			input: domain.UpdateBook{
+				Name_book: "Harry Potter",
+				Author:    "J. K. Rowling",
+			},
+			result:  domain.Book{},
+			wantErr: true,
+			repository: func(repo *m_repo.MockRepositoryPresenter) {
+				repo.EXPECT().GetBookByID("3").Return(domain.Book{
+					ID:        1,
+					Name_book: "Harry Potter",
+				}, nil)
+				repo.EXPECT().UpdateBook("3", domain.Book{
+					Name_book:  "Harry Potter",
+					Author:     "J. K. Rowling",
+					Updated_at: now,
+				}).Return(domain.Book{}, errors.New("failed to update database"))
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.nameTest, func(t *testing.T) {
+			repo := m_repo.NewMockRepositoryPresenter(ctrl)
+			if tt.repository != nil {
+				tt.repository(repo)
+			}
+
+			s := BookUseCase{
+				repoBook: repo,
+			}
+
+			book, err := s.UpdateBook(tt.id, tt.input)
+			if (err != nil) && tt.wantErr {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+			}
+			assert.Equal(t, tt.result, book)
+		})
+	}
+}
